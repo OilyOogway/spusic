feee29e0d031d637edc7ec2e2442516290568d54
OilyOogway <126415182+OilyOogway@users.noreply.github.com>
2023-12-10 11:52:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,114 +1,38 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
+const app = express();
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
-http.createServer((req, res) => {
-  const urlObj = url.parse(req.url, true);
-  const pathName = urlObj.pathname;
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
 
-  console.log('Requested path:', pathName);
-
-  if (pathName === '/') {
-    serveStaticFile(res, 'spusic.html');
-  } else if (pathName === '/index.js') {
-    serveStaticFile(res, 'index.js', 'text/javascript');
-  } else if (pathName === '/style.css') {
-    serveStaticFile(res, 'style.css', 'text/css');
-  } else if (pathName === "/process") {
-    // Implement your process logic here
-    // ...
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
-}).listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+// Route for the main page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'spusic.html'));
 });
 
-// Serve static files
-function serveStaticFile(res, fileName, contentType = 'text/html') {
-  const filePath = path.join(__dirname, 'public', fileName);
+// Route for serving index.js
+app.get('/index.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.js'));
+});
 
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Error reading file '${fileName}':`, err);
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error');
-      return;
-    }
+// Route for serving style.css
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'style.css'));
+});
 
-    res.writeHead(200, { 'Content-Type': contentType });
-    res.end(data);
-  });
-}
+// Implement your process logic here
+app.post('/process', (req, res) => {
+  // Implement your process logic here
+  // ...
+});
 
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
 
-
-
-
-
-// // MongoDB connection details
-// const Murl = "mongodb+srv://OilyOogway:Oilyasian@cluster0.tfq5hty.mongodb.net/?retryWrites=true&w=majority";
-// const http = require('http');
-// const url = require('url');
-// const MongoClient = require('mongodb').MongoClient;
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const dbName = 'Spusic';
-// const collectionName = 'highscore';
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // Middleware to parse JSON requests
-// app.use(bodyParser.json());
-
-
-// // Serve static files from the 'public' folder
-// app.use(express.static('public'));
-
-// // Route for the main page
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'spusic.html'));
-//   });
-
-// // Endpoint to submit a high score
-// app.post('/highscores', async (req, res) => {
-//   const { name, score } = req.body;
-
-//   try {
-//     const client = await MongoClient.connect(Murl, { useNewUrlParser: true, useUnifiedTopology: true });
-//     const db = client.db(dbName);
-//     const collection = db.collection(collectionName);
-
-//     await collection.insertOne({ name, score });
-
-//     res.status(201).json({ success: true });
-//   } catch (error) {
-//     console.error('Error inserting high score:', error);
-//     res.status(500).json({ success: false, error: 'Internal Server Error' });
-//   }
-// });
-
-// // Endpoint to get high scores
-// app.get('/highscores', async (req, res) => {
-//   try {
-//     const client = await MongoClient.connect(Murl, { useNewUrlParser: true, useUnifiedTopology: true });
-//     const db = client.db(dbName);
-//     const collection = db.collection(collectionName);
-//     console.log("we connected!")
-
-//     const highScores = await collection.find().sort({ score: -1 }).limit(10).toArray();
-
-//     res.status(200).json(highScores);
-//   } catch (error) {
-//     console.error('Error retrieving high scores:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
 
 // // Start the server
 // app.listen(port, () => {
