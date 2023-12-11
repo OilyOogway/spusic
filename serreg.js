068b368f9f415beb4efc66c04c0aec15cf7f874f
IanDhar <144154534+IanDhar@users.noreply.github.com>
2023-12-11 01:17:01 -0500

    adding redirect back to log in when account created

--- serreg.js
+++ serreg.js
@@ -78,6 +78,7 @@ app.post('/register', async (req, res) => {
     await newUser.save();
 
     res.send(`Account created successfully for ${username}!`);
+    return res.redirect('/login.html');
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
